db79e011f1b52db086c67281edf4917813327b21
Vitalik0422 <vitaliy19980422@gmail.com>
2026-03-23 20:30:04 +0200

    fix: third task

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,9 @@
 function getElementWidth(content, padding, border) {
-  return `Element Width with content: ${content}, padding: ${padding}, border: ${border} = ${
+  return (
     Number.parseFloat(content) +
     Number.parseFloat(padding) * 2 +
     Number.parseFloat(border) * 2
-  }`;
+  );
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
